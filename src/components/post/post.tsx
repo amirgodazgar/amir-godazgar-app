25b48cf9ac990da9ccc0946ac5169a2e6d13d26a
Amir Godazgar <amir.godazgar@connect2wow.com>
2023-11-28 19:05:01 +0330

    cleanup

--- src/components/post/post.tsx
+++ src/components/post/post.tsx
@@ -7,16 +7,13 @@ const Post = (props: Props) => {
   console.log("POST");
   const { title, description } = props;
   return (
-    <li className="text-md text-gray-900 w-[48rem]  min-h-[6rem] border rounded m-auto p-4 flex justify-between items-center h-full bg-white">
-      <div className="flex flex-col justify-start items-start w-[70%]">
-        <span className="text-lg font-bold text-gray-800 truncate w-[80%]">{title}</span>
-        <span className="text-md text-gray-500 w-[80%] truncate">
-          {description}
-        </span>
-      </div>
-      <div className="flex justify-center items-center w-[30%] h-full border">
-        icons
-      </div>
+    <li className="text-md text-gray-900 w-[48rem] min-h-[6rem] border rounded m-auto p-4 flex flex-col justify-start items-starth-full bg-white">
+      <span className="text-lg font-bold text-gray-800 truncate w-[90%]">
+        {title}
+      </span>
+      <span className="text-md text-gray-500 w-[80%] truncate">
+        {description}
+      </span>
     </li>
   );
 };
